2f0da5fdff6958995ddf2d173aabf7759faf95de
renjinyue <397751912@qq.com>
2017-02-03 00:34:48 +0800

    Delete album.js

--- js/album.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    var album = document.getElementsByClassName('album');
-    for (var i = 0; i < album.length; i++) {
-        album[i].onclick = function() {
-            window.location.href = "../html/album" + this.id + ".html";
-        }
-    }
-}
