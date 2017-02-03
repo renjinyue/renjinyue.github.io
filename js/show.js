fdcf90b0f833c7c9699d604938fc5e0d2a3f7a4e
renjinyue <397751912@qq.com>
2017-02-03 17:33:19 +0800

    Delete show.js

--- js/show.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    var src = window.location.search;
-    var oSrc = src.split("?");
-    var oImg = document.getElementById('img');
-    oImg.src = oSrc[1];
-    var download = document.getElementById('download');
-    download.download = oSrc[1];
-}
